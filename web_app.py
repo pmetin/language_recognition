2573f9bad4a0e10763241d7f793bfc43ecdd1f7d
pmetin <pmetpro@gmail.com>
2026-01-10 09:59:33 +0100

    Add exception if text < 0

--- web_app.py
+++ web_app.py
@@ -8,11 +8,12 @@ with st.form(key="my_form"):
     text_input = st.text_area("Enter your text here: ")
     submit_button = st.form_submit_button("Detect language")
 
-    if submit_button:
+    if submit_button and len(text_input) > 0:
         result = display_lang(text_input)
         st.success(result)
 
         stats = compute_stats(text_input)
+        # st.dataframe(pd.DataFrame(stats.items(), columns=["Metric", "Value"]))
         st.subheader("Statistics")
         st.write(stats)
         st.subheader("Visualization")
