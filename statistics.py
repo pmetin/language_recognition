d383893364706789b680d55d397c4f141c7a7f7e
pmetin <pmetpro@gmail.com>
2026-01-10 08:36:13 +0100

    Add stats() function

--- statistics.py
+++ statistics.py
@@ -1,3 +1,22 @@
-from typing import Any
+import re
 
-def stats(text: str) -> list[Any]
+def stats(text: str) -> str:
+    """
+    Generates statistics on a given text
+    Returns f-string that contains results
+    """
+    length_text = len(text)
+    words = text.split()
+    num_words = len(words)
+    sentences = re.split(r'[\.\?!]+', text)
+    num_sentences = len(sentences)
+    avg_len = sum(len(w) for w in words / num_words)
+
+    return f"""Stats: 
+Character count: {length_text}
+Word count: {num_words}
+Sentence count: {num_sentences}
+Average word length: {avg_len}
+"""
+
+
